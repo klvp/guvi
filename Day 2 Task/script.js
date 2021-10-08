e24bafaf101240821535f794dfb9983d15d41440
Lakshmi Vara Prasad Kuna <43074474+klvp@users.noreply.github.com>
2021-10-08 22:10:42 +0530

    Create script.js

---
+++ Day 2 Task/script.js
@@ -0,0 +1,22 @@
+console.log(typeof 1);
+console.log(typeof 1.1);
+console.log(typeof "1.1");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof NaN);
+
+
+
+/*Output:-
+number
+number
+string
+boolean
+object
+undefined
+object
+object
+number*/
